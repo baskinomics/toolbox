fbce858625e0f5ed7660d75a015c49a89d4979a6
Sean Baskin <sean.baskin@bnl-consulting.com>
2018-03-20 19:18:11 -0400

    Removed sudo references.

--- bash/getting-started.sh
+++ bash/getting-started.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Update
-sudo apt-get update
+apt update
 
 # Install Ansible
-sudo apt-get install --yes software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install --yes ansible
+apt install --yes software-properties-common
+apt-add-repository --yes --update ppa:ansible/ansible
+apt install --yes ansible
