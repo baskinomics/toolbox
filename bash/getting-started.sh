f66e0741899718969861b5cf9eceb5984f67aa01
Sean Baskin <seanbaskin@gmail.com>
2019-02-02 23:29:41 -0500

    Added some font installs and cleaned up

--- bash/getting-started.sh
+++ bash/getting-started.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-apt update
-apt upgrade -y
+apt-get update
+apt-get upgrade -y
 
-apt install -y \
+apt-get install -y \
   flashplugin-installer \
-  browser-plugin-freshplayer-pepperflash \
-  fonts-firacode
+  browser-plugin-freshplayer-pepperflash
+
+# TODO Bashit
+
+#-------------------------------------------------------------------------------
+# Fonts
+#-------------------------------------------------------------------------------
+# See: https://wiki.ubuntu.com/Fonts
+
+apt-get install fonts-firacode
+sudo -u ${USERNAME} curl --location --output ~/.local/share/fonts/segoeui.ttf https://github.com/meloncholy/mt-stats-viewer/blob/master/public/fonts/segoe-ui/segoeui.ttf?raw=true
+sudo -u ${USERNAME} fc-cache -f -v
