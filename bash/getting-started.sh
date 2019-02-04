9e8c137447e5be4d7839a1d2ea3bfc037ce485ad
Sean Baskin <seanbaskin@gmail.com>
2019-02-03 20:16:45 -0500

    Added gnome extensions and asciidoc

--- bash/getting-started.sh
+++ bash/getting-started.sh
@@ -3,17 +3,20 @@
 apt-get update
 apt-get upgrade -y
 
+# Package installation
 apt-get install -y \
   flashplugin-installer \
-  browser-plugin-freshplayer-pepperflash
+  browser-plugin-freshplayer-pepperflash \
+  asciidoc \
+  gnome-shell-extension-top-icons-plus \
+  gnome-shell-extension-caffeine \
+  gnome-shell-pomodoro \
+  gnome-shell-extension-weather
 
 # TODO Bashit
+# TODO Define gs alias
 
-#-------------------------------------------------------------------------------
-# Fonts
-#-------------------------------------------------------------------------------
-# See: https://wiki.ubuntu.com/Fonts
-
+# Fonts - See: https://wiki.ubuntu.com/Fonts
 apt-get install fonts-firacode
 sudo -u ${USERNAME} curl --location --output ~/.local/share/fonts/segoeui.ttf https://github.com/meloncholy/mt-stats-viewer/blob/master/public/fonts/segoe-ui/segoeui.ttf?raw=true
 sudo -u ${USERNAME} fc-cache -f -v
