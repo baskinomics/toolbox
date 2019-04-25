e85d545477ca346a96157c97adb0e271cbc68fd6
Sean Baskin <seanbaskin@gmail.com>
2019-04-25 17:13:44 -0400

    Initial Java bootstrap shell script

---
+++ bash/java.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install SDKMan!
+curl -s "https://get.sdkman.io" | bash
+
+# Install Java
+sdk install java 12.0.1-open
+
+# Install Maven
+sdk install maven
+
+# Install Gradle
+sdk install gradle
