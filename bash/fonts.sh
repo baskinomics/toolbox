2725934e2f57e87cb6d7b86cefb826309f67e5ef
Sean Baskin <seanbaskin@gmail.com>
2019-04-25 17:12:15 -0400

    Added Open Sans font

--- bash/fonts.sh
+++ bash/fonts.sh
@@ -1,5 +1,6 @@
 apt-get update
 apt-get install -y fonts-firacode
+apt-get install -y fonts-open-sans
 sudo -u ${USERNAME} curl --location --output ~/.local/share/fonts/segoeui.ttf https://github.com/meloncholy/mt-stats-viewer/blob/master/public/fonts/segoe-ui/segoeui.ttf?raw=true
 sudo -u ${USERNAME} curl --location --output ~/.local/share/fonts/HelveticaNeue.ttf https://raw.githubusercontent.com/adachis/Lifehacker.me/master/fonts/HelveticaNeue.ttf
 sudo -u ${USERNAME} curl --location --output ~/.local/share/fonts/helvetica.ttf https://raw.githubusercontent.com/CartoDB/cartodb/master/app/assets/fonts/helvetica.ttf
