da0a82291890cffdb503f1885d6f640c5d33e613
Sean Baskin <seanbaskin@gmail.com>
2019-04-28 21:07:18 -0400

    Updated go installation script functional now

--- bash/go.bash
+++ bash/go.bash
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-DOWNLOAD_DIR='/home/zoo/development/temp'
+# Logger
+source log.bash
 
-# Set version
-# TODO
+# Temporary download directory
+DOWNLOAD_DIR='/home/zoo/development/temp'
+GO_VERSION='go1.12.4'
+ARCH='linux-amd64'
 
 # Download the official Go binary
-printf "Downloading Go binary archive to $DOWNLOAD_DIR/go1.12.4.linux-amd64.tar.gz\n"
-curl --output "$DOWNLOAD_DIR/go1.12.4.linux-amd64.tar.gz" https://dl.google.com/go/go1.12.4.linux-amd64.tar.gz
+log "Downloading Go binary archive to $DOWNLOAD_DIR/$GO_VERSION.$ARCH.tar.gz"
+curl --silent --output "$DOWNLOAD_DIR/$GO_VERSION.$ARCH.tar.gz" https://dl.google.com/go/go1.12.4.linux-amd64.tar.gz
 
 # Extract the binary
-printf "Extracting Go binary archive to /user/local/go\n"
+log "Extracting Go binary archive to /user/local/go"
 tar -C /usr/local -xzf /home/zoo/development/temp/go1.12.4.linux-amd64.tar.gz
 
 # Cleanup
-printf "Removing binary archive\n"
-rm /home/zoo/development/temp/go1.12.4.linux-amd64.tar.gz
+log "Removing binary archive"
+rm $DOWNLOAD_DIR/$GO_VERSION.$ARCH.tar.gz
