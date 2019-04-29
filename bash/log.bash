aef8299b177c1338df2469f24177a8e5c60d7e7c
Sean Baskin <seanbaskin@gmail.com>
2019-04-28 21:09:32 -0400

    Added additional documentation

--- bash/log.bash
+++ bash/log.bash
@@ -4,6 +4,11 @@
 #   log()
 # Parameters:
 #   TEXT: The text to print to stdout
+# 
+# Example:
+#   log "Hello, World!"
+# 
+#   [log.bash] Hello, World!
 function log() {
     printf "[$0] $1\n"
 }
