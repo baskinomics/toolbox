cc6b9b9bae9e195dccca121535645f15ab752589
Sean Baskin <seanbaskin@gmail.com>
2019-04-29 14:55:54 -0400

    Added logic for creating custom GOPATH

--- bash/go.bash
+++ bash/go.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# set -x
+
 # Logger
 source log.bash
 
@@ -7,6 +9,7 @@ source log.bash
 DOWNLOAD_DIR='/home/zoo/development/temp'
 GO_VERSION='go1.12.4'
 ARCH='linux-amd64'
+GO_PATH='/home/zoo/development/git/go'
 
 # Download the official Go binary
 log "Downloading Go binary archive to $DOWNLOAD_DIR/$GO_VERSION.$ARCH.tar.gz"
@@ -19,3 +22,13 @@ tar -C /usr/local -xzf /home/zoo/development/temp/go1.12.4.linux-amd64.tar.gz
 # Cleanup
 log "Removing binary archive"
 rm $DOWNLOAD_DIR/$GO_VERSION.$ARCH.tar.gz
+
+# Make GOPATH directory
+log "Creating GOPATH directory"
+mkdir -p $GO_PATH
+mkdir -p $GO_PATH \
+    $GO_PATH/src \
+    $GO_PATH/pkg \
+    $GO_PATH/bin
+
+chown -R zoo:zoo $GO_PATH
