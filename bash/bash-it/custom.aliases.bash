86e139514631aea76e0aacf903de7cb5c839e146
Sean Baskin <seanbaskin@gmail.com>
2019-04-30 20:48:06 -0400

    Added bash-it custom aliases

---
+++ bash/bash-it/custom.aliases.bash
@@ -0,0 +1,8 @@
+alias gs='git status'
+alias ls='exa'
+alias ll='exa -la'
+alias lt='exa -laT -L 1'
+alias update='sudo apt update && apt list --upgradable'
+alias upgrade='sudo apt upgrade --yes'
+alias p='python3'
+alias todo="$TODO"
