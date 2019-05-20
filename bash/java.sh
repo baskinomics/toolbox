0898cd3844555e3af60209a351fa5cc7a72990e8
Sean Baskin <seanbaskin@gmail.com>
2019-05-20 15:14:29 -0400

    Updated to source after sdkman installation and added default java

--- bash/java.sh
+++ bash/java.sh
@@ -3,8 +3,14 @@
 # Install SDKMan!
 curl -s "https://get.sdkman.io" | bash
 
+# Source
+source "/home/zoo/.sdkman/bin/sdkman-init.sh"
+
 # Install Java
 sdk install java 12.0.1-open
+
+# Set Java 12 as default
+sdk default java 12.0.1-open
 
 # Install Maven
 sdk install maven
