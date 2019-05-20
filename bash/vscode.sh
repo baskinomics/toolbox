5de235de30abf9d031f51510a9ce0d8a13989031
Sean Baskin <seanbaskin@gmail.com>
2019-05-20 14:34:30 -0400

    Handle vscode extensions

---
+++ bash/vscode.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+code --install-extension be5invis.toml
+code --install-extension eddyvinck.Internetstorm
+code --install-extension ms-vscode.Go
+code --install-extension redhat.java
+code --install-extension rokoroku.vscode-theme-darcula
+code --install-extension rust-lang.rust
+code --install-extension vadimcn.vscode-lldb
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vscjava.vscode-java-debug
+code --install-extension vscjava.vscode-java-dependency
+code --install-extension vscjava.vscode-java-pack
+code --install-extension vscjava.vscode-java-test
+code --install-extension vscjava.vscode-maven
+code --install-extension webfreak.debug
