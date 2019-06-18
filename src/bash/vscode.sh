7e04247e44a6ee2e94e3bb812378b8ded3128b97
Sean Baskin <seanbaskin@gmail.com>
2019-06-17 20:07:23 -0400

    Refactored VSCode functions

--- src/bash/vscode.sh
+++ src/bash/vscode.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-code --install-extension be5invis.toml
-code --install-extension eddyvinck.Internetstorm
-code --install-extension ms-vscode.Go
-code --install-extension redhat.java
-code --install-extension rokoroku.vscode-theme-darcula
-code --install-extension rust-lang.rust
-code --install-extension vadimcn.vscode-lldb
-code --install-extension VisualStudioExptTeam.vscodeintellicode
-code --install-extension vscjava.vscode-java-debug
-code --install-extension vscjava.vscode-java-dependency
-code --install-extension vscjava.vscode-java-pack
-code --install-extension vscjava.vscode-java-test
-code --install-extension vscjava.vscode-maven
-code --install-extension webfreak.debug
+# Installs various VSCode extensions
+function install_vscode_extensions ()
+{
+    code --install-extension be5invis.toml
+    code --install-extension eddyvinck.Internetstorm
+    code --install-extension ms-vscode.Go
+    code --install-extension redhat.java
+    code --install-extension rokoroku.vscode-theme-darcula
+    code --install-extension rust-lang.rust
+    code --install-extension vadimcn.vscode-lldb
+    code --install-extension VisualStudioExptTeam.vscodeintellicode
+    code --install-extension vscjava.vscode-java-debug
+    code --install-extension vscjava.vscode-java-dependency
+    code --install-extension vscjava.vscode-java-pack
+    code --install-extension vscjava.vscode-java-test
+    code --install-extension vscjava.vscode-maven
+    code --install-extension webfreak.debug
+}
