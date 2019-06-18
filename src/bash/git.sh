33abdcc4bb8bf8119517411067afacb9a5ea04cc
Sean Baskin <seanbaskin@gmail.com>
2019-06-17 20:13:12 -0400

    Removed unnecessary variable call

--- src/bash/git.sh
+++ src/bash/git.sh
@@ -3,7 +3,7 @@
 # Sets various git config key-value pairs
 function configure_git ()
 {
-    ${USERNAME} git config --global user.name "Sean Baskin"
-    ${USERNAME} git config --global user.email "seanbaskin@gmail.com"
-    ${USERNAME} git config --global core.editor vim
+    git config --global user.name "Sean Baskin"
+    git config --global user.email "seanbaskin@gmail.com"
+    git config --global core.editor vim
 }
