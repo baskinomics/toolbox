7fa5f214f00d8757abd3967868e8d0ae811a162e
Sean Baskin <seanbaskin@gmail.com>
2019-06-29 01:40:58 -0400

    Added clap and initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use crate::tools::{bat, vim};
+use crate::fonts::hasklug;
+use crate::tools::{bat, lsd, vim};
+use clap::{App, Arg, ArgMatches, SubCommand};
 
 /// Represents different operating system package managers.
 pub mod package_manager {
@@ -41,10 +43,7 @@ pub mod package_manager {
 
 /// Contains a collection of functions that install various developer tools.
 ///
-/// # Notes
-///
-/// 1. Should make an enum or similar data structure to hold commands that will be used, i.e. `curl` or
-/// `dpkg`.
+/// todo Should make an enum or similar data structure to hold commands that will be used.
 pub mod tools {
     use std::process::{Command, Output};
 
@@ -119,8 +118,57 @@ pub mod tools {
     }
 }
 
+/// Contains a collection of functions that install various fonts.
+pub mod fonts {
+    use std::process::{Command, Output};
+
+    /// Installs the Hasklug Nerd Font font family.
+    pub fn hasklug() {
+        let release_url: String =
+            "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Hasklig.zip"
+                .parse()
+                .unwrap();
+
+        let curl: Output = Command::new("curl")
+            .args(&["-L", "-O", &release_url])
+            .output()
+            .expect("Failed to execute process.");
+
+        let unzip: Output = Command::new("unzio")
+            .args(&["Hasklig.zip", "-d", "~/.local/share/fonts"])
+            .output()
+            .expect("Failed to execute process.");
+
+        let rm: Output = Command::new("rm")
+            .arg("Hasklig.zip")
+            .output()
+            .expect("Failed to execute process.");
+
+        let fc_cache: Output = Command::new("unzio")
+            .args(&["Hasklig.zip", "-d", "/home/zoo/.local/share/fonts"])
+            .output()
+            .expect("Failed to execute process.");
+
+        println!("{}", String::from_utf8_lossy(&curl.stdout));
+        println!("{}", String::from_utf8_lossy(&unzip.stdout));
+        println!("{}", String::from_utf8_lossy(&rm.stdout));
+        println!("{}", String::from_utf8_lossy(&fc_cache.stdout));
+    }
+}
+
 /// Executes the various commands.
 fn main() {
-    vim();
-    bat();
+    let commands: ArgMatches = App::new("monastery")
+        .version("0.0.1")
+        .author("Sean Baskin <seanbaskin@gmail.com>")
+        .about("Opinionated yet configurable tool to manage your development environment.")
+        .get_matches();
+
+    // Install tools
+    //vim();
+    //bat();
+    //lsd();
+
+    // Install fonts
+    //hasklug();
 }
