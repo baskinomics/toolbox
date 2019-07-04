f0699c02808db7483ef539ceb6d9dd78b61fed96
Sean Baskin <seanbaskin@gmail.com>
2019-07-03 22:08:19 -0400

    Added installation of Source Sans Pro and refactored a bit

--- src/bash/fonts.sh
+++ src/bash/fonts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Installs Nerd Fonts
+# todo implement
 function install_nerd_fonts ()
 {
     # https://github.com/ryanoasis/nerd-fonts/releases/tag/v2.0.0
@@ -11,15 +12,43 @@ function install_nerd_fonts ()
     # sudo -u ${USERNAME} curl --location --output ~/.local/share/fonts/helvetica.ttf https://raw.githubusercontent.com/CartoDB/cartodb/master/app/assets/fonts/helvetica.ttf
     # sudo -u ${USERNAME} curl --location --output ~/.local/share/fonts/Lucida%20Grande.ttf https://raw.githubusercontent.com/guardianproject/securereader/master/app/src/main/assets/fonts/Lucida%20Grande.ttf
 
-    # Build fonts
-    fc-cache -f -v
+    # Build font information caches
+    fc-cache --force --verbose
 }
 
-# Installs miscellaneous fonts
-function install_misc_fonts ()
+# Installs fonts available via the apt package manager
+function install_font_apt_available ()
 {
-    apt-get install -y fonts-open-sans
+    # Open Sans
+    apt-get install --yes fonts-open-sans
 
-    # Build fonts
-    fc-cache -f -v
+    # Microsoft Core Fonts
+    apt-get install --yes ttf-mscorefonts-installer
+
+    # Build font information caches
+    # todo is this needed or does apt execute this during the installation process?
+    fc-cache --force --verbose
+}
+
+# Installs the Source Sans Pro font family
+function install_font_source_sans_pro ()
+{
+    # Define the release URL
+    release="https://github.com/adobe-fonts/source-sans-pro/releases/download/2.045R-ro%2F1.095R-it/source-sans-pro-2.045R-ro-1.095R-it.zip"
+    
+    # cURL the release artifact from GitHub
+    curl --remote-name --location $release
+
+    # Unzip the archive to a temporary directory
+    unzip source-sans-pro-2.045R-ro-1.095R-it.zip -d temp-source-sans-pro
+
+    # Extract the TTF font files
+    cp temp-source-sans-pro/source-sans-pro-2.045R-ro-1.095R-it/OTF/* /home/zoo/.local/share/fonts
+
+    # Build font information caches
+    fc-cache --force --verbose
+
+    # Cleanup
+    rm source-sans-pro-2.045R-ro-1.095R-it.zip
+    rm -rf temp-source-sans-pro
 }
