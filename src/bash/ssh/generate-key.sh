095b392d258552ecb97c9950436faa44c07d8df3
Sean Baskin <seanbaskin@gmail.com>
2019-10-09 11:53:41 -0400

    Added function documentation

--- src/bash/ssh/generate-key.sh
+++ src/bash/ssh/generate-key.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Name: 
+#   generate-ssh-key
+# Parameters:
+# 
+# Description:
+#   Generates a 4096-bit RSA key for the current user and pipes the output 
+#   of `cat` to the system clip via `xclip`.
 function generate-ssh-key() {
     ssh-keygen -t rsa -b 4096
     cat -pp ~/.ssh/id_rsa.pub | xclip -sel clip
